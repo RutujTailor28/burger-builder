4aaea3824337aa22fe3b6e65a89ee622f89ca7d5
RutujTailors <nisargtailor21@gmail.com>
2019-09-09 21:59:32 +0530

    generated dynamic burger using firebase-db

--- src/component/UI/Backdrop/Backdrop.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import './Backdrop.css'
-const backDrop = (props) => (
-    props.showed ? <div className='Backdrop' onClick={props.clicked}></div> : null
-);
-
-export default backDrop;
